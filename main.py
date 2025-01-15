0481a2f5ec0badd06939bc852506f6c6414c71c3
Hyongyi <102146170+Hyongyi@users.noreply.github.com>
2025-01-15 23:58:47 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from service.openAI import router as openAi_router
 from service.words import router as words_router
 from fastapi.middleware.cors import CORSMiddleware
 
+
 app = FastAPI()
 router = APIRouter()
 
